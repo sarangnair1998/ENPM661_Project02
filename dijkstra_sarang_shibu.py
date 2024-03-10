5bfeccfbd0ee4cc5269304209c289e9a10c5d3c3
sarangnair1998 <sarangshibu98@gmail.com>
2024-03-10 17:25:18 -0400

    check if (x,y) in obstacle or map

--- dijkstra_sarang_shibu.py
+++ dijkstra_sarang_shibu.py
@@ -58,6 +58,8 @@ def draw_regular_polygon_with_clearance(canvas, color, vertex_count, radius, pos
     
     return vertices
 
+
+
 def obstacles_map(canvas):
     
     cv2.polylines(canvas, [np.array([(0, 0), (1200, 0), (1200, 500), (0, 500)])], isClosed=True, color=(0, 0, 255), thickness=5)
@@ -85,3 +87,42 @@ def obstacles_map(canvas):
     
     return canvas
 
+
+def check_obstacle_map(x, y):
+    hexagon1_center = (650, 250)
+    hexagon1_radius = 150
+    hexagon1_rotation = 90
+    clearance = 5
+    vertices1 = draw_regular_polygon_with_clearance(canvas, (0, 0, 255), 6, hexagon1_radius, hexagon1_center, hexagon1_rotation,clearance)
+    obstacle_polygons = [
+        [(95, 405), (180, 405), (180, 0), (95, 0)],
+        [(270, 95), (355, 95), (355, 500), (270, 500)],
+        vertices1,
+        [(895, 380), (1025, 380), (1025, 120), (895, 120),
+         (895, 55), (1095, 55), (1095, 445), (895, 445)]
+    ]
+    for obstacle in obstacle_polygons:
+        if cv2.pointPolygonTest(np.array(obstacle), (x, y), False) >= 0:
+            return True
+    return False
+
+def check_coordinates():    
+    start_x = int(input("Enter the X coordinate of the start position: "))
+    start_y = int(input("Enter the Y coordinate of the start position: "))
+    goal_x = int(input("Enter the X coordinate of the goal position: "))
+    goal_y = int(input("Enter the Y coordinate of the goal position: "))
+
+    if not (5 <= start_x <= 1195 and 5 <= start_y <= 495):
+        print("The start position is out of bounds or within the clearance area.")
+        return None
+    if not (5 <= goal_x <= 1195 and 5 <= goal_y <= 495):
+        print("The goal position is out of bounds or within the clearance area.")
+        return None
+    
+    if check_obstacle_map(start_x, start_y) or check_obstacle_map(goal_x, goal_y):
+        print("Start or goal position falls within an obstacle.")
+        return None
+    else:
+        print("Start and goal positions are not obstructed by obstacles.")
+    
+        return [start_x, start_y], [goal_x, goal_y]

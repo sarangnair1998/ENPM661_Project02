b19b89b58c1a2d9c86b005fc02806424d90994e7
sarangnair1998 <sarangshibu98@gmail.com>
2024-03-10 17:19:11 -0400

    Implemented Obstacle Space
    Implement obstacles in the map. Write a function to generate the hexagon obstacle

--- dijkstra_sarang_shibu.py
+++ dijkstra_sarang_shibu.py
@@ -43,3 +43,45 @@ def move_node(node, canvas, xdot, ydot):
         return True, next_node
     else:
         return False, node
+    
+def draw_regular_polygon_with_clearance(canvas, color, vertex_count, radius, position, rotation=0, clearance=0,thickness = -1):
+    n = vertex_count
+    r = radius + clearance 
+    x, y = position
+    rotation_rad = pi / 180 * rotation 
+
+    vertices = []
+    for i in range(n):
+        x_i = x + r * cos(rotation_rad + 2 * pi * i / n)
+        y_i = y + r * sin(rotation_rad + 2 * pi * i / n)
+        vertices.append((int(x_i), int(y_i)))
+    
+    return vertices
+
+def obstacles_map(canvas):
+    
+    cv2.polylines(canvas, [np.array([(0, 0), (1200, 0), (1200, 500), (0, 500)])], isClosed=True, color=(0, 0, 255), thickness=5)
+    
+    cv2.fillPoly(canvas, pts = [np.array([(100,400), (175,400), (175,0), (100,0)])], color = (255, 0, 0))
+    cv2.polylines(canvas, pts = [np.array([(100,400), (175,400), (175,0), (100,0)])],isClosed=True, color=(0, 0, 255), thickness=5)
+    
+    cv2.fillPoly(canvas, pts = [np.array([(275,100), (350,100), (350,500), (275,500)])], color = (255, 0, 0))
+    cv2.polylines(canvas, pts = [np.array([(275,100), (350,100), (350,500), (275,500)])],isClosed=True, color=(0, 0, 255), thickness=5)
+    
+    hexagon1_center = (650, 250)
+    hexagon1_radius = 150
+    hexagon1_rotation = 90
+    clearance1 = 0
+    clearance2 = 5
+    vertices1 = draw_regular_polygon_with_clearance(canvas, (0, 0, 255), 6, hexagon1_radius, hexagon1_center, hexagon1_rotation, clearance1)
+    vertices2 = draw_regular_polygon_with_clearance(canvas, (0, 0, 255), 6, hexagon1_radius, hexagon1_center, hexagon1_rotation, clearance2)
+    cv2.fillPoly(canvas, pts = [np.array(vertices1)], color = (255, 0, 0))
+    cv2.polylines(canvas, pts = [np.array(vertices2)],isClosed=True, color=(0, 0, 255), thickness=5)
+    
+    cv2.fillPoly(canvas, pts = [np.array([(900,375), (1020,375), (1020,125), (900,125),(900,50),(1100,50),
+                                          (1100,450),(900,450)])], color = (255, 0, 0))
+    cv2.polylines(canvas, pts = [np.array([(900,375), (1020,375), (1020,125), (900,125),(900,50),(1100,50),
+                                          (1100,450),(900,450)])],isClosed=True, color=(0, 0, 255), thickness=5)
+    
+    return canvas
+
